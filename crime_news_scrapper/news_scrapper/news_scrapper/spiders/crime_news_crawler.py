dbac95de498195556c1651877e6598f0725728a3
Madhura Vora <mandemadhura@gmail.com>
2025-01-22 08:57:19 +0000

    Crime News Scrapper: Make website URLs and domains configurable #12
    - Update yaml config for spider with websites URL to scrap with its additional
      required config
    - Read config from yaml and initiate the spider execution

--- crime_news_scrapper/news_scrapper/news_scrapper/spiders/crime_news_crawler.py
+++ crime_news_scrapper/news_scrapper/news_scrapper/spiders/crime_news_crawler.py
@@ -1,35 +1,33 @@
+import logging
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 from news_scrapper.parsers.india_today_parser import IndiaTodayParser
 from news_scrapper.parsers.indian_express_parser import IndianExpressParser
+from config import load_config
 
 
 class CrimeNewsSpider(CrawlSpider):
+    CONFIG_PATH = "/home/madhura/projects/pangolin/crime_news_scrapper/config.yaml"
     name = "news_spider"
-
-    # allow localhost in order to serve request for splash server running on localhost
-    allowed_domains = ["indiatoday.in", "indianexpress.com", "localhost"]
-    start_urls = ["https://www.indiatoday.in/crime", "https://www.indianexpress.com/about/crime-news"]
-
-    # Follow links from each response extracted from indiatoday.in/crime page and
-    # indianexpress.com/about/crime-news page
-    # Fetch unique results only hence unique=True 
-    rules = (Rule(
-                    LinkExtractor(allow=(r"https://www\.indiatoday\.in/crime(/.*)?$"),
-                                unique=True,),
-                                follow=True),
-             Rule(
-                    LinkExtractor(allow=(r"https://www\.indianexpress\.com/about/crime-news/"),
-                                   unique=False,),
-                                   follow=True),
-
-    )
 
     def __init__(self):
         super().__init__()
         self.india_today = IndiaTodayParser()
         self.indian_express = IndianExpressParser()
+        self.rules_list = []
+        # allow localhost in order to serve request for splash server running on localhost
+        self.allowed_domains = ["localhost"]
+        config = load_config(file_path=self.CONFIG_PATH)
+        self.config = config.get("spider", {})
+        for domain, domain_config in self.config.items():
+            self.allowed_domains.append(domain)
+            self.start_urls.append(domain_config.get("start_url", ""))
+            self.rules_list.append(Rule(LinkExtractor(allow=(domain_config.get("allow", "")),
+                                                    unique=domain_config.get("unique", True,)),
+                                                    follow=domain_config.get("follow", True),),)
+        self.rules = tuple(self.rules_list)
 
 
     def parse_start_url(self, response):
